a04a222480cfb20bd5c6144cc9cd61355fa35d3e
Hen0k <henoktilaye7@gmail.com>
2022-05-24 09:27:03 +0300

    impr: PEP8 formatting

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -7,6 +7,7 @@ from src.rotating_logs import get_rotating_log
 
 logger = get_rotating_log(filename='data_loader.log')
 
+
 class DataLoader:
     """This class is a wrapper for getting DVC versioned datasets and normal csvs from file"""
     @staticmethod
@@ -16,20 +17,20 @@ class DataLoader:
             path: str -> file path starting from root of repo
             version: str -> a git tag or a git commit hash where the file exists
             repo: str -> file directory or link for repository containing the versioned dataset
-        
+
         Returns:
             pd.DataFrame
         """
         try:
             content = dvc.api.read(path=path,
-                                repo=repo,
-                                rev=version)
+                                   repo=repo,
+                                   rev=version)
             df = pd.read_csv(io.StringIO(content), sep=",")
-            logger.info(f"DVC: CSV file read with path: {path} | version: {version} | from: {repo}")
+            logger.info(
+                f"DVC: CSV file read with path: {path} | version: {version} | from: {repo}")
             return df
         except:
             logger.exception("DVC data getter raised an exception")
-        
 
     @staticmethod
     def read_csv(path: str) -> pd.DataFrame:
--- src/rotating_logs.py
+++ src/rotating_logs.py
@@ -3,9 +3,7 @@ import time
 import os
 
 from logging.handlers import RotatingFileHandler
-
 from fsutil import split_path
-
 
 
 # Helper methods
@@ -23,24 +21,24 @@ def get_root_dir() -> None:
                 return '/'.join(folders[:idx])
 
 
-
 def get_rotating_log(filename: str) -> logging.Logger:
     """Creates a rotating log object that writes to a file and the console"""
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    
+
     repo_root = get_root_dir()
     log_file_path = os.path.join(repo_root, 'logs', filename)
     # add a rotating handler
-    rot_handler = RotatingFileHandler(log_file_path, 
-                                maxBytes=1000000,
-                                backupCount=1)
+    rot_handler = RotatingFileHandler(log_file_path,
+                                      maxBytes=1000000,
+                                      backupCount=1)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
     c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     console_handler.setFormatter(c_format)
     rot_handler.setFormatter(f_format)
@@ -48,7 +46,4 @@ def get_rotating_log(filename: str) -> logging.Logger:
     logger.addHandler(rot_handler)
     logger.addHandler(console_handler)
 
-    return logger 
-
-
-
+    return logger
