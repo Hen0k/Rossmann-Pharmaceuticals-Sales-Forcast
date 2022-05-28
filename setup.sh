d1d7811ef0b3b52591547e40b7006c6393021635
Hen0k <henoktilaye7@gmail.com>
2022-05-28 19:43:45 +0300

    dashboard: add heroku deployment helpers

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "[email protected]"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
