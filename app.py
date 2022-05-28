318db4e9b4b8bf6891c96d2352a99edbc55a371e
Hen0k <henoktilaye7@gmail.com>
2022-05-28 22:27:20 +0300

    dashboard: Add file uploader

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
 
 
-st.header("Rossmann Pharmaceuticals Sales Forcaster")
+st.image('images/ROSSMANN.jpg')
+st.header("Rossmann Pharmaceuticals Sales Forecaster")
+
+input_data = st.file_uploader(label="Upload a CSV or excel file",
+                              type=['csv', 'xlsx'],
+                              accept_multiple_files=False)
+
+if input_data is not None:
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(input_data)
+    st.write(dataframe)
